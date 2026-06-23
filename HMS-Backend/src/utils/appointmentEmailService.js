bba20ada4bdeec92789da0bdcd43debd56337621
Tanguturi Venkata Lakshmi Ajay <113990108+ajaytanguturi@users.noreply.github.com>
2026-06-23 23:35:04 +0530

    Delete HMS-Backend directory

--- HMS-Backend/src/utils/appointmentEmailService.js
+++
@@ -1,3 +0,0 @@
-const { sendAppointmentConfirmationEmail, sendAppointmentCancellationEmail, sendAppointmentReminderEmail } = require('./emailService');
-
-module.exports = { sendAppointmentConfirmationEmail, sendAppointmentCancellationEmail, sendAppointmentReminderEmail };
