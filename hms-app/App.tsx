23b8f336a4633d19771857ef0bc24ebbeea9907b
Tanguturi Venkata Lakshmi Ajay <113990108+ajaytanguturi@users.noreply.github.com>
2026-06-23 23:35:30 +0530

    Delete hms-app directory

--- hms-app/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AppNavigation from './src/navigations/AppNavigation';
-
-function App() {
-  return (<AppNavigation />)
-}
-export default App;
--- hms-app/src/config/apiConfig.tsx
+++
@@ -1,6 +0,0 @@
-import { Platform } from "react-native";
-export const API_BASE_URL = Platform.select({
-    android: 'http://10.0.2.2:5000/api',
-    ios: 'http://localhost:5000/api',
-    default: 'http://localhost:5000/api',
-})
